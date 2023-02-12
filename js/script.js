789d10b12a99271055de38f81d27823cff37cdf3
Vitaliy Parhomenko <vt.parhomenko@gmail.com>
2023-02-13 01:12:35 +0200

    mobile menu

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
 "use strict"
 
 
-let menuBtn = document.querySelector('.menu-btn');
-let menu = document.querySelector('.menu');
-menuBtn.addEventListener('click', function () {
-	menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
-});
+// let menuBtn = document.querySelector('.menu-btn');
+// let menu = document.querySelector('.menu');
+// menuBtn.addEventListener('click', function () {
+// 	menuBtn.classList.toggle('active');
+// 	menu.classList.toggle('active');
+// });
 
 //search form clickable btn
 let search_form = document.getElementById("search_form");
 search_form.querySelector(".section__submit img").addEventListener("click", e => {
 	console.log( 'submit form')
 	search_form.submit();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	new Mmenu("#top-menu", {		theme		: "white",
+		offCanvas	: {
+			position	: "bottom"
+		},
+		navbars		: [
+
+			{
+			content : ["prev","title"]
+		}]
+	}, {});
+
 });
