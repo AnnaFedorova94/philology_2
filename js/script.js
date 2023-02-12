a85e70409c041db8324bdbae4d895e2913e9805f
Vitaliy Parhomenko <vt.parhomenko@gmail.com>
2023-02-13 00:21:25 +0200

    clickable search btn and gitignore file

--- js/script.js
+++ js/script.js
@@ -7,3 +7,10 @@ menuBtn.addEventListener('click', function () {
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 });
+
+//search form clickable btn
+let search_form = document.getElementById("search_form");
+search_form.querySelector(".section__submit img").addEventListener("click", e => {
+	console.log( 'submit form')
+	search_form.submit();
+});
