73da78825b1af8967b37584d8df29fc8f185ea53
Vitaliy Parhomenko <vt.parhomenko@gmail.com>
2023-02-14 00:13:29 +0200

    menu v2

--- js/script.js
+++ js/script.js
@@ -2,11 +2,17 @@
 
 
 let menuBtn = document.querySelector('.menu-btn');
-let menu = document.querySelector('.menu');
+let menu = document.querySelector('.header__nav');
 menuBtn.addEventListener('click', function () {
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 });
+
+let parent_menu_item = menu.querySelector( ".menu-item-has-children" )
+
+parent_menu_item.addEventListener( 'click', e => {
+	e.target.classList.toggle( "active" );
+})
 
 //search form clickable btn
 let search_form = document.getElementById("search_form");
